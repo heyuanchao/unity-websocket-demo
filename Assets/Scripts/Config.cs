3d63f169f56b70f27d77d45522c4c49df00b5948
heyuanchao <1205386762@qq.com>
2018-09-10 17:47:59 +0800

    提交

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -3,8 +3,8 @@
     private const string localServHost = "192.168.11.24";
     private const string remoteServHost = "game.hedr.top";
 
-    private const string gameServHost = localServHost;
-    // private const string gameServHost = remoteServHost;
+    // private const string gameServHost = localServHost;
+    private const string gameServHost = remoteServHost;
 
     public const string gameServWSAddr = "ws://" + gameServHost + ":8000";
     public const string gameServHTTPAddr = "http://" + gameServHost + ":8001";
